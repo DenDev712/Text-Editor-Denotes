7bb9e2a362a84c479c86e2ed1df30e92c8e568c6
GintoBos <114522869+GintoBos@users.noreply.github.com>
2024-08-23 18:13:46 +0200

    message will disappear after 5 second

--- src/editor/messagebar.rs
+++ src/editor/messagebar.rs
@@ -1,22 +1,46 @@
-use std::io::Error;
+use std::{io::Error, time::Duration, time::Instant};
 
 use super::{
     terminal::{Size, Terminal},
     uicomponent::UIComponent,
 };
+//the message duration before hiding
+const DEFAULT_DURATION: Duration = Duration::new(5, 0);
+struct Message{
+    text: String,
+    time: Instant,    
+}
 
+impl Default for Message{
+    fn default() -> Self {
+        Self{
+            text: String::new(),
+            time: Instant::now(),
+        }
+    }
+}
+
+impl Message{
+    fn has_expired(&self) -> bool {
+        Instant::now().duration_since(self.time) > DEFAULT_DURATION
+    }
+}
 #[derive(Default)]
 pub struct MessageBar{
-    current_message: String,
+    current_message: Message,
     needs_redraw: bool,
+    cleared_expired_message: bool,
 }
 
 impl MessageBar{
     pub fn update_message(&mut self, new_message: String){
-        if new_message != self.current_message{
-            self.current_message = new_message;
-            self.mark_redraw(true);
-        }
+        self.current_message = Message{
+            text: new_message,
+            time: Instant::now(),
+        };
+
+        self.cleared_expired_message = false;
+        self.mark_redraw(true);
     }
 }
 
@@ -26,12 +50,21 @@ impl UIComponent for MessageBar{
     }
 
     fn needs_redraw(&self) -> bool{
+        (!self.cleared_expired_message && self.current_message.has_expired()) || 
         self.needs_redraw
     }
 
     fn set_size(&mut self, _: Size){}
 
     fn draw(&mut self, origin: usize) -> Result<(), Error>{
-        Terminal::print_row(origin, &self.current_message)
+        if self.current_message.has_expired(){
+            self.cleared_expired_message = true;
+        }
+        let message = if self.current_message.has_expired() {
+            ""
+        }else{
+            &self.current_message.text
+        };
+        Terminal::print_row(origin, message)
     }
 }
