b10e65a682f37ceafe4373a1f2925092f487bdbd
GintoBos <114522869+GintoBos@users.noreply.github.com>
2024-08-27 19:03:14 +0200

    The message bar will now show if there was an error or if the action was successful

--- src/editor.rs
+++ src/editor.rs
@@ -5,7 +5,7 @@ use std::{
     panic::{set_hook, take_hook},
 };
 mod documentstatus;
-mod editorcommand;
+mod command;
 mod fileinfo;
 mod messagebar;
 mod statusbar;
@@ -13,16 +13,23 @@ mod uicomponent;
 mod terminal;
 mod view;
 use documentstatus::DocumentStatus;
-use editorcommand::EditorCommand;
 use statusbar::StatusBar;
 use uicomponent::UIComponent;
 use terminal::Terminal;
 use view::View;
 
-use self::{messagebar::MessageBar, terminal::Size};
+use self::{
+    command::{
+        Command::{self, Edit, Move, System},
+        System::{Quit, Resize, Save},
+    },
+    messagebar::MessageBar,
+    terminal::Size,
+};
 pub const NAME: &str = env!("CARGO_PKG_NAME");
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+const QUIT_TIME: u8 = 3;
 #[derive(Default)]
 pub struct Editor {
     should_quit: bool,
@@ -31,6 +38,7 @@ pub struct Editor {
     title: String,
     message_bar: MessageBar,
     terminal_size: Size,
+    quit_times: u8,
 }
 
 impl Editor {
@@ -44,16 +52,21 @@ impl Editor {
         let mut editor = Self::default();
         let size = Terminal::size().unwrap_or_default();
         editor.resize(size);
+        editor
+            .message_bar
+            .update_message("HELP: Ctrl-S to Save | Ctrl-Q to Quit");
 
 
         let args: Vec<String> = env::args().collect();
         //if a file name is given then load that said file 
         if let Some(file_name) = args.get(1){
-            editor.view.load(file_name);
+            if editor.view.load(file_name).is_err(){
+                editor
+                    .message_bar
+                    .update_message(&format!("ERR: Could not open file: {file_name}"));
+            }
         }
-        editor
-            .message_bar
-            .update_message("Help: Ctrl-S to save | Ctrl-Q to quit".to_string());
+        
         editor.refresh_status();
         Ok(editor)
     }
@@ -101,7 +114,7 @@ impl Editor {
         }
     }
 
-    #[allow(clippy::needless_pass_by_value)]
+    
     fn evaluate_event(&mut self, event: Event) {
         let should_process = match &event {
             Event::Key(KeyEvent { kind, .. }) => kind == &KeyEventKind::Press,
@@ -109,15 +122,52 @@ impl Editor {
             _ => false,
         };
         if should_process {
-            if let Ok(command) = EditorCommand::try_from(event) {
-                if matches!(command, EditorCommand::Quit) {
-                    self.should_quit = true;
-                } else if let EditorCommand::Resize(size) = command{
-                    self.resize(size);
-                }else{
-                    self.view.handle_command(command);
-                }
+            if let Ok(command) = Command::try_from(event) {
+                self.process_command(command);
             }
+        }
+    }
+
+    fn process_command(&mut self, command: Command) {
+        match command{
+            System(Quit) => self.handle_quit(),
+            System(Resize(size)) => self.resize(size),
+            _ => self.reset_quit_times(),
+        }
+
+        match command{
+            System(Quit | Resize(_)) => {}
+            System(Save) => self.handle_save(),
+            Edit(edit_command) => self.view.handle_edit_command(edit_command),
+            Move(move_command) => self.view.handle_move_command(move_command),
+        }
+    }
+
+    fn handle_save(&mut self){
+        if self.view.save().is_ok(){
+            self.message_bar.update_message("File Saved");
+        }else{
+            self.message_bar.update_message("File could not be Saved");
+        }
+    }
+
+    #[allow(clippy::arithmetic_side_effects)]
+    fn handle_quit(&mut self){
+        if !self.view.get_status().is_modified || self.quit_times + 1 == QUIT_TIME{
+            self.should_quit = true;
+        }else if self.view.get_status().is_modified{
+            self.message_bar.update_message(&format!(
+                "File has unsaved changes. Press Ctrl-Q {} more times to quit",
+                QUIT_TIME - self.quit_times - 1
+            ));
+            self.quit_times += 1;
+        }
+    }
+
+    fn reset_quit_times(&mut self){
+        if self.quit_times > 0{
+            self.quit_times = 0;
+            self.message_bar.update_message("");
         }
     }
     fn refresh_screen(&mut self) {
