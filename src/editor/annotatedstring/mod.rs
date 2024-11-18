3a64c9077b5037bdd30278ab445f7335e83ac0e9
GintoBos <114522869+GintoBos@users.noreply.github.com>
2024-11-18 19:42:15 +0100

    small changes

--- src/editor/annotatedstring/mod.rs
+++ src/editor/annotatedstring/mod.rs
@@ -2,7 +2,6 @@ use std::{
     cmp::{max, min},
     fmt::{self, Display},
 };
-
 pub mod annotationtype;
 pub use annotationtype::AnnotationType;
 mod annotation;
@@ -11,6 +10,7 @@ mod annotatedstringpart;
 use annotatedstringpart::AnnotatedStringPart;
 mod annotatedstringiterator;
 use annotatedstringiterator::AnnotatedStringIterator;
+
 use super::ByteIdx;
 
 #[derive(Default, Debug)]
@@ -18,7 +18,6 @@ pub struct AnnotatedString {
     string: String,
     annotations: Vec<Annotation>,
 }
-
 impl AnnotatedString {
     pub fn from(string: &str) -> Self {
         Self {
@@ -31,15 +30,14 @@ impl AnnotatedString {
         annotation_type: AnnotationType,
         start: ByteIdx,
         end: ByteIdx,
-    ){
+    ) {
         debug_assert!(start <= end);
-        sself.annotations.push(Annotation {
+        self.annotations.push(Annotation {
             annotation_type,
             start,
             end,
         });
     }
-
     pub fn truncate_left_until(&mut self, until: ByteIdx) {
         self.replace(0, until, "");
     }
@@ -55,16 +53,17 @@ impl AnnotatedString {
         }
         self.string.replace_range(start..end, new_string);
 
-        let replaced_range_len = end.saturating_sub(start);
+        let replaced_range_len = end.saturating_sub(start); // This is the range we want to replace.
         let shortened = new_string.len() < replaced_range_len;
-        let len_difference = new_string.len().abs_diff(replaced_range_len); 
+        let len_difference = new_string.len().abs_diff(replaced_range_len); // This is how much longer or shorter the new range is.
 
         if len_difference == 0 {
+            //No adjustment of annotations needed in case the replacement did not result in a change in length.
             return;
         }
 
         self.annotations.iter_mut().for_each(|annotation| {
-            annotation.start = if annotation.start >= end{
+            annotation.start = if annotation.start >= end {
                 // For annotations starting after the replaced range, we move the start index by the difference in length.
                 if shortened {
                     annotation.start.saturating_sub(len_difference)
@@ -74,15 +73,9 @@ impl AnnotatedString {
             } else if annotation.start >= start {
                 // For annotations starting within the replaced range, we move the start index by the difference in length, constrained to the beginning or end of the replaced range.
                 if shortened {
-                    max(
-                        start,
-                        annotation.start.saturating_sub(len_difference),
-                    )
+                    max(start, annotation.start.saturating_sub(len_difference))
                 } else {
-                    min(
-                        end,
-                        annotation.start.saturating_add(len_difference),
-                    )
+                    min(end, annotation.start.saturating_add(len_difference))
                 }
             } else {
                 annotation.start
@@ -98,36 +91,29 @@ impl AnnotatedString {
             } else if annotation.end >= start {
                 // For annotations ending within the replaced range, we move the end index by the difference in length, constrained to the beginning or end of the replaced range.
                 if shortened {
-                    max(
-                        start,aannotation.end.saturating_sub(len_difference)))
+                    max(start, annotation.end.saturating_sub(len_difference))
                 } else {
-                    min(
-                        end,aannotation.end.saturating_add(len_difference))
+                    min(end, annotation.end.saturating_add(len_difference))
                 }
             } else {
-                annotation.
-            };
-            
+                annotation.end
+            }
         });
 
         //Filter out empty annotations, in case the previous step resulted in any.
         self.annotations.retain(|annotation| {
-            annotation.start < annotation.end
-                && annotation.start < self.string.len()
+            annotation.start < annotation.end && annotation.start < self.string.len()
         });
     }
 }
-
 impl Display for AnnotatedString {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         write!(formatter, "{}", self.string)
     }
 }
-
 impl<'a> IntoIterator for &'a AnnotatedString {
     type Item = AnnotatedStringPart<'a>;
     type IntoIter = AnnotatedStringIterator<'a>;
-
     fn into_iter(self) -> Self::IntoIter {
         AnnotatedStringIterator {
             annotated_string: self,
